d09eb6734c05d6e1d7c09e31f1f0017123d314a9
김유진 <calathea@yujin-kim.local>
2021-09-24 15:46:35 +0900

    First commit

---
+++ src/test/java/SymmetricEncrypterTest.java
@@ -0,0 +1,31 @@
+import com.encrypter.Encrypter;
+import com.encrypter.SymmetricEncrypter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class SymmetricEncrypterTest {
+    private Encrypter encrypter = new SymmetricEncrypter("61731D3B905CBF544268119046E0A986");
+
+    @Test
+    @Disabled
+    public void generateAndPrintKey() {
+        SymmetricEncrypter.generateAndPrintKey();
+    }
+
+    @Test
+    public void encryptAndDecrypt() {
+        //given
+        String target = "I'm korean.";
+        String targetKor = "나는 대한민국 사람입니다.";
+
+        //when
+        String encTarget = encrypter.encrypt(target);
+        String encTargetKor = encrypter.encrypt(targetKor);
+        String decTarget = encrypter.decrypt(encTarget);
+        String decTargetKor = encrypter.decrypt(encTargetKor);
+
+        Assertions.assertEquals(target, decTarget);
+        Assertions.assertEquals(targetKor, decTargetKor);
+    }
+}
